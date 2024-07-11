a69368556e3cd4a847bc689d1cb768c808d6e655
Shaykhnazar <shayx3470941@gmail.com>
2024-07-11 22:22:42 +0500

    upgraded laravel version to 11.x

--- app/Models/User.php
+++ app/Models/User.php
@@ -99,4 +99,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class, 'user_id');
     }
+
+    public function getAuthPasswordName()
+    {
+        return 'password';
+    }
 }
