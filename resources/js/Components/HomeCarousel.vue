1b51cf2a47fff27f7e99815533c6684d28f5bf3e
Shaykhnazar <shayx3470941@gmail.com>
2024-08-31 14:24:17 +0500

    changelog: - carousel fixes
    1. npm install

--- resources/js/Components/HomeCarousel.vue
+++ resources/js/Components/HomeCarousel.vue
@@ -11,27 +11,27 @@ onMounted(() => {
 })
 
 const carouselItems = ref([
-    {
-      id: 1,
-      image_url: `https://startinvestuz-190827817.imgix.net/carousel_1.jpg?q=80&w=1920&auto=format&fit=crop`,
-      title: 'G\'oya',
-      short: 'G\'oyalaringiz bilan o\'rtoqlashing!',
-      route: 'ideas.index'
-    },
-    {
-      id: 2,
-      image_url: `https://startinvestuz-190827817.imgix.net/carousel_2.jpg?q=80&w=1920&auto=format&fit=crop`,
-      title: 'Startup',
-      short: 'Keyingi jamoa a\'zolaringizni toping va mahoratingizni oshiring!',
-      route: 'startups.index'
-    },
-    {
-      id: 3,
-      image_url: `https://startinvestuz-190827817.imgix.net/carousel_3.jpg?q=80&w=1920&auto=format&fit=crop`,
-      title: 'Investitsiya',
-      short: 'Eng yaxshi deb hisoblagan startap loyihani izlab toping va uni moliyalashtiring!',
-      route: 'startups.index'
-    },
+  {
+    id: 1,
+    image_url: `https://startinvestuz-190827817.imgix.net/carousel_2.jpg?q=80&w=1920&auto=format&fit=crop`,
+    title: 'Startup',
+    short: 'Keyingi jamoa a\'zolaringizni toping va mahoratingizni oshiring!',
+    route: 'startups.index'
+  },
+  {
+    id: 2,
+    image_url: `https://startinvestuz-190827817.imgix.net/carousel_1.jpg?q=80&w=1920&auto=format&fit=crop`,
+    title: 'G\'oya',
+    short: 'G\'oyalaringiz bilan o\'rtoqlashing!',
+    route: 'ideas.index'
+  },
+  {
+    id: 3,
+    image_url: `https://startinvestuz-190827817.imgix.net/carousel_3.jpg?q=80&w=1920&auto=format&fit=crop`,
+    title: 'Investitsiya',
+    short: 'Eng yaxshi deb hisoblagan startap loyihani izlab toping va uni moliyalashtiring!',
+    route: 'startups.index'
+  },
 ])
 </script>
 
@@ -41,7 +41,8 @@ const carouselItems = ref([
     <div data-hs-carousel='{
       "loadingClasses": "opacity-0",
       "isAutoplay": true,
-      "isInfiniteLoop": true
+      "isInfiniteLoop": true,
+      "speed": 3000
     }' class="relative">
       <div class="hs-carousel relative overflow-hidden w-full h-[30rem] md:h-[calc(100vh-106px)]  bg-gray-100 rounded-2xl dark:bg-neutral-800">
         <div class="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
@@ -67,7 +68,7 @@ const carouselItems = ref([
       </div>
 
       <!-- Arrows -->
-      <button type="button" class="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-12 h-full text-black hover:bg-white/20 rounded-s-2xl focus:outline-none focus:bg-white/20">
+      <button type="button" class="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-12 h-full text-white hover:bg-white/20 rounded-s-2xl focus:outline-none focus:bg-white/20">
       <span class="text-2xl" aria-hidden="true">
         <svg class="shrink-0 size-3.5 md:size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path>
@@ -76,7 +77,7 @@ const carouselItems = ref([
         <span class="sr-only">Oldingi</span>
       </button>
 
-      <button type="button" class="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-12 h-full text-black hover:bg-white/20 rounded-e-2xl focus:outline-none focus:bg-white/20">
+      <button type="button" class="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-12 h-full text-white hover:bg-white/20 rounded-e-2xl focus:outline-none focus:bg-white/20">
         <span class="sr-only">Keyingi</span>
         <span class="text-2xl" aria-hidden="true">
         <svg class="shrink-0 size-3.5 md:size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
