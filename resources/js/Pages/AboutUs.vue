f614a886af800a796b5f2d2e9c190191a55bb45e
Shaykhnazar <shayx3470941@gmail.com>
2024-09-03 20:53:59 +0500

    changelog: - about us new design

--- resources/js/Pages/AboutUs.vue
+++ resources/js/Pages/AboutUs.vue
@@ -1,61 +1,66 @@
 <script setup>
 
 import App from '@/Layouts/App.vue'
-import {Head} from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 </script>
 
 <template>
   <App>
     <template #header>
-      <Head title="About"/>
+      <Head title="Biz haqimizda"/>
     </template>
 
-    <div class="p-6 bg-white shadow-md rounded-md">
+    <el-container>
+      <div class="max-w-[85rem] px-4 sm:px-6 lg:px-8 mx-auto">
+        <div class="grid lg:grid-cols-3 gap-y-8 lg:gap-y-0 lg:gap-x-6">
+          <!-- Content -->
+          <div class="lg:col-span-2">
+            <div class="py-8 lg:pe-8">
+              <div class="space-y-5 lg:space-y-8">
+                <Link class="inline-flex items-center gap-x-1.5 text-sm text-gray-600 decoration-2 hover:underline focus:outline-none focus:underline dark:text-blue-500" :href="route('home')">
+                  <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+                  Asosiy sahifa
+                </Link>
 
-      <h1 class="text-2xl font-semibold mb-4">🚀 Startinvest.uz haqida</h1>
-      <p class="text-gray-700 leading-relaxed mb-4">
-        🚀 Startinvest.uz - bu Startuplarni rivojlantirish, investitsiya jalb qilish uchun online platforma!<br>
+                <h2 class="text-3xl font-bold lg:text-5xl dark:text-white">Startinvest.uz haqida</h2>
 
-        <q> Sizda agarda go’ya yoki startup qurish va uni moliyalashtirish istagi bor bo’lsa ushbu platforma orqali uni amalga oshirishingiz mumkin!</q><br>
-        <q>Agarda siz investor bo'lsangiz, u holda bizning platformadagi kelajagi porloq deb hisoblangan startuplarga investorlik qilish imkoniyatiga ega bo'lasiz!</q>
-      </p>
+                <div class="text-lg text-gray-800 dark:text-neutral-200">
+                  <p class="text-gray-700 leading-relaxed mb-4">
+                    🚀 Startinvest.uz - bu Startuplarni rivojlantirish, investitsiya jalb qilish uchun online platforma!<br>
 
-      <div class="mb-6">
-        <h2 class="text-xl font-semibold mb-2">Platformaning asosiy maqsadi va afzalliklari ❔:</h2>
-        <p class="text-gray-700 leading-relaxed" style="text-indent: 20px">
-          Agarda sizda MVP formatidagi startup loyihangiz bo’lsa uni moliyalashtirish uchun bizning platformadan foydalangan holda investitsiya jalb qilishingiz mumkin bo’ladi.
-          Bizdagi investorlar sizning go’yangizni kuzatishi va agarda ularga maq’ul kelsa ularga investitsiya qilishi mumkin.
-          Agarda sizda faqatgina go’ya yoki amaliyot o’tash istagi bo’lsa unda siz o’zingizni qiziqtirgan g’oya ustida jamoa to’plashingiz va uni MVP versiyada tayyorlab bizning platformada e’lon qilish orqali loyihangizga investorlarni jalb qilishingiz mumkin.
-        </p>
+                    <q>Sizda agarda go’ya yoki startup qurish va uni moliyalashtirish istagi bor bo’lsa ushbu platforma orqali uni amalga oshirishingiz mumkin!</q><br>
+                    <q>Agarda siz investor bo'lsangiz, u holda bizning platformadagi kelajagi porloq deb hisoblangan startuplarga investorlik qilish imkoniyatiga ega bo'lasiz!</q>
+                  </p>
+
+                  <div class="mb-6">
+                    <h2 class="text-xl font-semibold mb-2">Platformaning asosiy maqsadi va afzalliklari ❔:</h2>
+                    <div class="text-gray-700 leading-relaxed" style="text-indent: 20px">
+                      <ul class="list-disc list-inside text-gray-700 leading-relaxed mb-4">
+                        <li>Agarda sizda MVP formatidagi startup loyihangiz bo’lsa uni moliyalashtirish uchun bizning platformadan foydalangan holda investitsiya jalb qilishingiz mumkin bo’ladi.</li>
+                        <li>Bizning platformada investorlar sizning go’yangizni kuzatishi va agarda ularga maq’ul kelsa ularga investitsiya kiritishi mumkin.</li>
+                        <li>Agarda sizda faqatgina go’ya yoki amaliyot o’tash istagi bo’lsa unda siz o’zingizni qiziqtirgan g’oya ustida jamoa to’plashingiz va uni MVP versiyada tayyorlab, platformada orqali e’lon qilish orqali loyihangizga investorlarni jalb qilishingiz mumkin.</li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div class="mb-6">
+                    <h2 class="text-xl font-semibold mb-2">Plaftormadagi foydalanuvchi rollari 🕵️:</h2>
+                    <div class="text-gray-700 leading-relaxed">
+                      <ul class="list-disc list-inside text-gray-700 leading-relaxed mb-4">
+                        <li>Go’ya ulashuvchilar - shunchaki hayoliga kelgan go’yani ulashadigan foydalanuvchilar (sababi ular shu go’yani o’zlari amalga oshirishga imkoniyati va salohiyati bo’lmasligi lekin uni kimdir amalga oshirishini hohlovchi kimsalar)</li>
+                        <li>Dasturchilar, loyiha menedjerlari(PM), jamoa yetakchilari(Team lead)…</li>
+                        <li>Investorlar - kompaniya, tadbirkorlar, katta/o'rta/kichik biznes egalari</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div class="mb-6">
-        <h2 class="text-xl font-semibold mb-2">Plaftormadagi foydalanuvchi rollari 🕵️:</h2>
-        <p class="text-gray-700 leading-relaxed">
-          <ul class="list-disc list-inside text-gray-700 leading-relaxed mb-4">
-            <li>Oddiy go’ya egasi - shunchaki hayoliga kelgan go’yani ulashadigan oddiy foydalanuvchilar (sababi ular shu go’yani o’zlari amalga oshirishga imkoniyati va salohiyati bo’lmasligi lekin uni kimdir amalga oshirishini hohlovchi kimsalar)</li>
-            <li>dasturchilar, Loyiha menedjerlari(PM), Jamoa yetakchilari(Team lead)…</li>
-            <li>Investorlar - kompaniyalar, tadbirkorlar, katta/o'rta/kichik biznes egalari</li>
-          </ul>
-        </p>
-      </div>
-
-<!--      <div>-->
-<!--        <h2 class="text-xl font-semibold mb-2">ChatGPT qanday fikrda 😎:</h2>-->
-<!--        <p class="text-gray-700 leading-relaxed">-->
-<!--          Platformaning asosiy afzalliklari orasida:-->
-<!--        </p>-->
-<!--        <ul class="list-disc list-inside text-gray-700 leading-relaxed mb-4">-->
-<!--          <li>G'oyalar va resurslarni samarali birlashtirish imkoniyati.</li>-->
-<!--          <li>Innovatsion loyihalarni moliyalashtirish va ularni rivojlantirishda yordam berish.</li>-->
-<!--          <li>Turli soha mutaxassislarini bir joyda to'plash orqali yangi hamkorlik va tarmoqlarni yaratish.</li>-->
-<!--        </ul>-->
-<!--        <p class="text-gray-700 leading-relaxed">-->
-<!--          Bu kabi platforma ko'plab yosh tadbirkorlar va startupchilar uchun juda foydali bo'lishi mumkin, chunki u ularning g'oyalarini haqiqatga aylantirishda zarur bo'lgan resurslar va bilimlarga kirish imkonini beradi. Shuningdek, bu kabi platforma moliyaviy yordam va maslahatlar bilan ta'minlash orqali ularning loyihalarini yanada tezroq va samarali amalga oshirishlariga yordam beradi.-->
-<!--        </p>-->
-<!--      </div>-->
-    </div>
+    </el-container>
   </App>
 </template>
 
